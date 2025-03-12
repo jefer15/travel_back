56d7e7d1a7dd1600e84fadde5b90513260ea38bf
jefer15 <70409281+jefer15@users.noreply.github.com>
2025-03-12 17:44:21 -0500

    feat: add jwt and user and auth models

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Carbon\Carbon;
+
+class AuthController extends Controller
+{
+    // Registro de usuario
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json(['message' => 'Usuario registrado con éxito'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Credenciales incorrectas'], 401);
+        }
+
+        $user = auth()->user();
+
+        $customClaims = [
+            'name' => $user->name,
+            'email' => $user->email,
+            'exp' => Carbon::now()->addDays(7)->timestamp
+        ];
+
+        // Generar un nuevo token con los claims personalizados
+        $token = JWTAuth::claims($customClaims)->fromUser($user);
+
+        return response()->json([
+            'message' => 'Login exitoso',
+            'token' => $token
+        ]);
+    }
+
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        JWTAuth::invalidate(JWTAuth::getToken());
+        return response()->json(['message' => 'Sesión cerrada']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware(['jwt'])->group(function () {
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
+
+    // Route::get('/weather', [WeatherController::class, 'getWeather']);
+    // Route::get('/exchange-rate', [CurrencyController::class, 'getExchangeRate']);
+    // Route::get('/history', [HistoryController::class, 'getHistory']);
+
